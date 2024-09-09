e39fc4c3895a5c46b62e3d5a706ebf8dcb8bb6f1
Abdul ELrahman A.Bahget <abosama150@gmail.com>
2024-09-09 17:50:28 +0300

    add UI Componnents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppComponent } from "./app.component";
+import { TicketCardComponent } from "./ticket-card/ticket-card.component";
+import { ChartCardComponent } from "./chart-card/chart-card.component";
+import { HeaderComponent } from "./header/header.component";
+import { TableComponent } from "./table/table.component";
 
 @NgModule({
     declarations: [
         AppComponent,
+        HeaderComponent,
+        TicketCardComponent,
+        ChartCardComponent,
+        TableComponent
     ],
     imports: [
         BrowserModule,
