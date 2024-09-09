907c25b91cf007f1281fee87493ec4d2454e71c5
Abdul ELrahman A.Bahget <abosama150@gmail.com>
2024-09-09 17:48:06 +0300

    init chart-card, ticket-card, table components

---
+++ src/app/chart-card/chart-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chart-card',
+  templateUrl: './chart-card.component.html',
+  styleUrl: './chart-card.component.scss'
+})
+export class ChartCardComponent {
+
+}
---
+++ src/app/ticket-card/ticket-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket-card',
+  templateUrl: './ticket-card.component.html',
+  styleUrl: './ticket-card.component.scss'
+})
+export class TicketCardComponent {
+
+}
